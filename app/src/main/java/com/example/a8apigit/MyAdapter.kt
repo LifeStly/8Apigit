0e01e8d2a70a9b081df7e31d7260a2d4d4947d73
LifeStly <tattonman001@gmail.com>
2020-09-30 01:11:08 +0700

    create Myadapter

---
+++ app/src/main/java/com/example/a8apigit/MyAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.a8apigit
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a8apigit.model.Data
+
+class MyAdapter (private val dataList:MutableList<Data>) : RecyclerView.Adapter<>()
